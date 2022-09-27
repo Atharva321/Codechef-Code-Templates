9309f79e0d642942fb1347a8cc101bb5717b89fa
Atharva Patil <44301928+Atharva321@users.noreply.github.com>
2022-09-27 13:27:21 +0530

    Update InOut2.java

--- General Code Template/InOut2.java
+++ General Code Template/InOut2.java
@@ -1,1 +1,63 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
 
+/* Name of the class has to be "Main" only if the class is public. */
+class Codechef
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		// your code goes here
+		InputReader ir = new InputReader(System.in);
+        int t = ir.nextInt();
+        while(t-->0){
+        }
+        ir.close();
+	}
+}
+
+class InputReader{
+    private BufferedReader br;
+    InputReader(InputStream is){
+        br = new BufferedReader(new InputStreamReader(is));
+    }
+    private StringTokenizer st;
+    public String nextLine(){
+        String s=null;
+        try {
+            s = br.readLine();
+        } catch (Exception e) {
+           
+        }
+        return s;
+    }
+    public String next(){
+        if(st == null || !st.hasMoreTokens()){
+            try {
+                st = new StringTokenizer(br.readLine());
+            } catch (Exception e) {
+                System.out.println(e.getMessage());
+            }
+        }
+        return st.nextToken(" ");
+    }
+
+    public int nextInt(){
+        return Integer.parseInt(next());
+    }
+
+    public long nextLong(){
+        return Long.parseLong(next());
+    }
+
+    public double nextDouble(){
+        return Double.parseDouble(next());
+    }
+    public void close(){
+       try {
+        br.close();
+       } catch (IOException e) {
+        System.out.println(e.getMessage());
+       }
+    }
+}
